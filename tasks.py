58b63cdafc5ff5011d40d5d186f117b6d459b0a4
ProszetYang <yang@proszet.com>
2024-05-15 18:01:12 +0900

    Adding tokko login

--- tasks.py
+++ tasks.py
@@ -1,6 +1,22 @@
 from robocorp.tasks import task
+from robocorp import windows
+from robocorp import vault
 
+desktop = windows.desktop()
 @task
 def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+    # login_tokko()
+    tokko_main = windows.find_window('subname:"特攻店長 - メインメニュー"')
+    tokko_main.find("id:pbOrderList").click()
+    
+    tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
+    tokko_order.inspect()
+
+
+def login_tokko():
+    desktop.windows_run("C:\Program Files (x86)\TokkoTencho\特攻店長\特攻店長.exe")
+    login = vault.get_secret("tokko")
+    tokko_login = windows.find_window('subname:"特攻店長 - ログイン"')
+    tokko_login.find("id:txtUserID").set_value(login["id"])
+    tokko_login.find("id:txtPassword").set_value(login["password"], validator = None)
+    tokko_login.find("id:btnLogin").click()
