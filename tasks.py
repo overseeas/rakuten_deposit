e704ba6dde5ea42a49db8a38872253fc4fe0e63d
overseeas <overseeas@gmail.com>
2024-05-23 17:29:35 +0900

    Add initializing search details

--- tasks.py
+++ tasks.py
@@ -5,12 +5,11 @@ from robocorp import vault
 desktop = windows.desktop()
 @task
 def minimal_task():
-    # login_tokko()
-    tokko_main = windows.find_window('subname:"特攻店長 - メインメニュー"')
-    tokko_main.find("id:pbOrderList").click()
-    
-    tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
-    tokko_order.inspect()
+    #login_tokko()
+    #tokko_main = windows.find_window('subname:"特攻店長 - メインメニュー"')
+    #tokko_main.find("id:pbOrderList").click()
+    set_search_config()
+
 
 
 def login_tokko():
@@ -20,3 +19,23 @@ def login_tokko():
     tokko_login.find("id:txtUserID").set_value(login["id"])
     tokko_login.find("id:txtPassword").set_value(login["password"], validator = None)
     tokko_login.find("id:btnLogin").click()
+
+def set_search_config():
+    tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
+    #initialize status
+    #tokko_order.select("", locator="id:cmbStatus")
+    #tokko_order.find("name:詳細検索▼").click()
+    detailed_options = tokko_order.find_many("id:cmbSearchType", search_strategy="all", search_depth=6)
+    detailed_options = detailed_options[:-1]
+
+    detail_option(detailed_options)
+    
+
+    #name:詳細検索▼ id:btnChangeSearchDetail
+    """
+    for i in combobox:
+        print(i)
+    """
+
+def detail_option(options):
+    options[0].select("・処理状況（テキスト）")
