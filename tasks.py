39fd4bc2084d84907c9c975c0fbb0146978a58d6
overseeas <overseeas@gmail.com>
2024-05-30 17:59:57 +0900

    Add search settings and seperate tokko modules

--- tasks.py
+++ tasks.py
@@ -1,41 +1,21 @@
+import sys
+sys.path.append("C:/Users/LINK_RPA_001/Desktop/robocorp/modules")
+print(sys.path)
 from robocorp.tasks import task
 from robocorp import windows
 from robocorp import vault
+import Tokko_Tencho.main
+import Tokko_Tencho.order
 
 desktop = windows.desktop()
 @task
 def minimal_task():
-    #login_tokko()
-    #tokko_main = windows.find_window('subname:"特攻店長 - メインメニュー"')
-    #tokko_main.find("id:pbOrderList").click()
-    set_search_config()
-
-
-
-def login_tokko():
-    desktop.windows_run("C:\Program Files (x86)\TokkoTencho\特攻店長\特攻店長.exe")
+    """
     login = vault.get_secret("tokko")
-    tokko_login = windows.find_window('subname:"特攻店長 - ログイン"')
-    tokko_login.find("id:txtUserID").set_value(login["id"])
-    tokko_login.find("id:txtPassword").set_value(login["password"], validator = None)
-    tokko_login.find("id:btnLogin").click()
-
-def set_search_config():
-    tokko_order = windows.find_window('subname:"特攻店長 - 受注一覧"')
-    #initialize status
-    #tokko_order.select("", locator="id:cmbStatus")
-    #tokko_order.find("name:詳細検索▼").click()
-    detailed_options = tokko_order.find_many("id:cmbSearchType", search_strategy="all", search_depth=6)
-    detailed_options = detailed_options[:-1]
-
-    detail_option(detailed_options)
-    
-
-    #name:詳細検索▼ id:btnChangeSearchDetail
+    Tokko_Tencho.main.login_tokko(login["id"], login["password"])
+    Tokko_Tencho.main.select_menu("受注一覧")
+    Tokko_Tencho.order.initialize()
+    Tokko_Tencho.order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+    Tokko_Tencho.order.option_list(1, "楽天ステータス", "発送待ち")
     """
-    for i in combobox:
-        print(i)
-    """
-
-def detail_option(options):
-    options[0].select("・処理状況（テキスト）")
+    order_nums = Tokko_Tencho.order.search()
