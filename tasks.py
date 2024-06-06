e80de32b7fa7cfab66b60e7d960fe14f1dc8a2b8
overseeas <overseeas@gmail.com>
2024-06-06 17:43:40 +0900

    Finished, waiting for the test

--- tasks.py
+++ tasks.py
@@ -1,41 +1,76 @@
 import sys
-sys.path.append("C:/Users/LINK_RPA_001/Desktop/robocorp/modules")
+sys.path.append("C:/Users/LINK_RPA_001/Desktop/robocorp/modules/Tokko_Tencho")
 print(sys.path)
 from robocorp.tasks import task
 from robocorp import windows
 from robocorp import vault
-import Tokko_Tencho.Main
-import Tokko_Tencho.Order
+import Tokko_Tencho
 import datetime
 
 desktop = windows.desktop()
 @task
 def minimal_task():
+    login_info = vault.get_secret("tokko")
 
-    today = datetime.datetime.now()
-    
-    """login = vault.get_secret("tokko")
-    print("<")
-    Tokko_Tencho.Main.open()
-    print(">")
-    Tokko_Tencho.Main.login(login["id"], login["password"])
-    Tokko_Tencho.Main.select_menu("受注一覧")
-    Tokko_Tencho.Order.initialize()
-    Tokko_Tencho.Order.option_text(0, "・処理状況（テキスト）", "入金待ち")
-    Tokko_Tencho.Order.option_list(1, "楽天ステータス", "発送待ち")
-    if Tokko_Tencho.Order.search() > 0:
+    menu = Tokko_Tencho.Main()
+    menu.open()
+    menu.login(login_info["id"], login_info["password"])
+    menu.select_menu("受注一覧")
+
+    order = Tokko_Tencho.Order()
+    order.initialize()
+    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+    order.option_list(1, "楽天ステータス", "発送待ち")
+    if order.search() > 0:
+        orders_list = order.get_values_from_list("ID")
+        orders = ", ".join(map(str, orders_list))
         #Send mail
-        Tokko_Tencho.Order.list_all_click()
-        Tokko_Tencho.Order.send_mails("入金完了メール")
+        order.list_all_click()
+        order.send_mails("入金完了メール")
 
         #発注分類：受注発注
-        Tokko_Tencho.Order.option_text(2,"発注分類","受注発注")
-        if Tokko_Tencho.Order.search() > 0:
-            Tokko_Tencho.Order.list_all_click()"""
-    order_window = windows.find_window('subname:"特攻店長 - 受注一覧"')
-    order_window.find("id:btnEditPlural").click()
-    subwindow = order_window.find_child_window("id:FrmOrderBatchUpdater")
-    subwindow.find("id:cbEditStatus").click()
-    subwindow.find("id:cmbStatus").select("入金済み")
-    subwindow.find("id:cbActionLog").click()
-    subwindow.find("id:txtActionLog").set_value(datetime.date.strftime(datetime.datetime.now(), "%m/%d　発送待ち確認"))
+        status_change(orders, order, "受注発注")
+        #発注分類：直送
+        status_change(orders, order, "直送")
+        #発注分類：在庫品
+        status_change_direct(orders, order)
+
+    menu.close()
+
+
+
+def status_change(orders, order, option):
+    order.initialize()
+    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+    order.option_list(1, "楽天ステータス", "発送待ち")
+    order.input_order_id(orders)
+    order.option_text(0,"・発注分類", option)
+    if order.search() > 0:
+        order.list_all_click()
+        subwindow = order.open_bulk_change()
+        subwindow.find("id:cbEditStatus").click()
+        subwindow.find("id:cmbStatus").select("入金済み")
+        confirming_change(order, subwindow)
+
+def status_change_direct(orders, order):
+    order.initialize()
+    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+    order.option_list(1, "楽天ステータス", "発送待ち")
+    order.input_order_id(orders)
+    if order.search() > 0:
+        order.list_all_click()
+        subwindow = order.open_bulk_change()
+        subwindow.find("id:cbEditStatus").click()
+        subwindow.find("id:cmbStatus").select("出荷日確定")
+        subwindow.find("id:cbEditShipmentDueDate").click()
+        confirming_change(order, subwindow)
+
+def confirming_change(order, window):
+    window.find("id:cbActionLog").click()
+    window.find("id:txtActionLog").set_value(datetime.date.strftime(datetime.datetime.now(), "%m/%d　発送待ち確認"))
+    window.find("id:btnEditPlural").click()
+    order.wait()
+    order.find("control:ButtonControl class:Button path:1|1|1").click()
+    order.wait()
+    order.find("control:ButtonControl class:Button path:1|1|1").click()
+    order.wait()
