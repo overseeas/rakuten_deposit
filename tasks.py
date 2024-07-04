3ac98c26d87c9d8c5b7e09078c5cd7a3e7e8898e
overseeas <overseeas@gmail.com>
2024-07-04 18:00:31 +0900

    Add assertion

--- tasks.py
+++ tasks.py
@@ -15,57 +15,70 @@ desktop = windows.desktop()
 def minimal_task():
     login_info = vault.get_secret("tokko")
     
+    bank = False
+    convinience = False
 
     #銀行
     menu = Tokko_Tencho.Main()
     menu.open()
-    menu.login(login_info["id"], login_info["password"])
-    menu.select_menu("受注一覧")
+    try:
+        menu.login(login_info["id"], login_info["password"])
+        menu.select_menu("受注一覧")
 
-    order = Tokko_Tencho.Order()
-    order.initialize()
-    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
-    order.option_list(1, "楽天ステータス", "発送待ち")
-    order.input_order_number("r")
-    if order.search() > 0:
-        orders_list = order.get_values_from_list("ID")
-        orders = ", ".join(map(str, orders_list))
+        order = Tokko_Tencho.Order()
+        order.initialize()
+        order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+        order.option_list(1, "楽天ステータス", "発送待ち")
+        order.input_order_number("r")
+        if order.search() > 0:
+            orders_list = order.get_values_from_list("ID")
+            orders = ", ".join(map(str, orders_list))
 
-        #発注分類：受注発注
-        status_change(orders, order, "受注発注")
-        #発注分類：直送
-        status_change(orders, order, "直送")
-        #発注分類：在庫品
-        status_change_direct(orders, order)
+            #発注分類：受注発注
+            status_change(orders, order, "受注発注")
+            #発注分類：直送
+            status_change(orders, order, "直送")
+            #発注分類：在庫品
+            status_change_direct(orders, order)
+        bank = True
+    except:
+        pass
     menu.close()
     
 
     #コンビニ
     menu = Tokko_Tencho.Main()
     menu.open()
-    menu.login(login_info["id"], login_info["password"])
-    menu.select_menu("受注一覧")
+    try:
+        menu.login(login_info["id"], login_info["password"])
+        menu.select_menu("受注一覧")
 
-    order = Tokko_Tencho.Order()
-    order.initialize()
-    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
-    order.option_list(1, "楽天ステータス", "発送待ち")
-    if order.search() > 0:
-        orders_list = order.get_values_from_list("ID")
-        orders = ", ".join(map(str, orders_list))
-        #Send mail
-        order.list_all_click()
-        order.send_mails("入金完了メール")
+        order = Tokko_Tencho.Order()
+        order.initialize()
+        order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+        order.option_list(1, "楽天ステータス", "発送待ち")
+        if order.search() > 0:
+            orders_list = order.get_values_from_list("ID")
+            orders = ", ".join(map(str, orders_list))
+            #Send mail
+            order.list_all_click()
+            order.send_mails("入金完了メール")
 
-        #発注分類：受注発注
-        status_change(orders, order, "受注発注")
-        #発注分類：直送
-        status_change(orders, order, "直送")
-        #発注分類：在庫品
-        status_change_direct(orders, order)
+            #発注分類：受注発注
+            status_change(orders, order, "受注発注")
+            #発注分類：直送
+            status_change(orders, order, "直送")
+            #発注分類：在庫品
+            status_change_direct(orders, order)
+        convinience = True
+    except:
+        pass
     menu.close()
 
-    mailto("chumon@maido-diy.jp", "★完了報告★楽天入金処理完了報告", "楽天の銀行、コンビニ払いの入金処理しました。")
+    if convinience and bank:
+        mailto("yang@proszet.com", "★完了報告★楽天入金処理完了報告", "楽天の銀行、コンビニ払いの入金処理しました。")
+    else:
+        mailto("yang@proszet.com", "★エラー報告★楽天入金処理完了報告", "楽天の銀行、コンビニ払いの入金処理に失敗しました。")
 
 def mailto(to, subject, body):
     secrets = vault.get_secret("Mail")
@@ -73,7 +86,7 @@ def mailto(to, subject, body):
     app.open_application()
     app.send_email(
         recipients=to,
-        cc_recipients=["yang@proszet.com", "funaki@proszet.com"],
+        #cc_recipients=["yang@proszet.com", "funaki@proszet.com"],
         subject=subject,
         body=body
     )
@@ -109,7 +122,9 @@ def confirming_change(order, window):
     window.find("id:txtActionLog").set_value(datetime.date.strftime(datetime.datetime.now(), "%m/%d　発送待ち確認"))
     window.find("id:btnEditPlural").click()
     order.wait()
+    assert window.find("control:ButtonControl class:Button path:1|1")
     window.find("control:ButtonControl class:Button path:1|1").click()
     order.wait()
+    assert window.find("control:ButtonControl class:Button path:1|1")
     window.find("control:ButtonControl class:Button path:1|1").click()
     order.wait()
