96d8d3c29652a332e62dffc18ec0884fdddd482f
overseeas <overseeas@gmail.com>
2024-07-11 09:25:21 +0900

    Fix search status

--- tasks.py
+++ tasks.py
@@ -8,7 +8,7 @@ import datetime
 from robocorp import log
 from RPA.Email.ImapSmtp import ImapSmtp
 from RPA.Outlook.Application import Application
-
+import traceback
 
 desktop = windows.desktop()
 @task
@@ -35,14 +35,14 @@ def minimal_task():
             orders = ", ".join(map(str, orders_list))
 
             #発注分類：受注発注
-            status_change(orders, order, "受注発注")
+            status_change(orders, order, "受注発注", "入金待ち")
             #発注分類：直送
-            status_change(orders, order, "直送")
+            status_change(orders, order, "直送", "入金待ち")
             #発注分類：在庫品
             status_change_direct(orders, order)
         bank = True
     except:
-        pass
+        traceback.print_tb()
     menu.close()
     
 
@@ -55,7 +55,7 @@ def minimal_task():
 
         order = Tokko_Tencho.Order()
         order.initialize()
-        order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+        order.option_text(0, "・処理状況（テキスト）", "収納依頼")
         order.option_list(1, "楽天ステータス", "発送待ち")
         if order.search() > 0:
             orders_list = order.get_values_from_list("ID")
@@ -65,14 +65,14 @@ def minimal_task():
             order.send_mails("入金完了メール")
 
             #発注分類：受注発注
-            status_change(orders, order, "受注発注")
+            status_change(orders, order, "受注発注", "収納依頼")
             #発注分類：直送
-            status_change(orders, order, "直送")
+            status_change(orders, order, "直送", "収納依頼")
             #発注分類：在庫品
-            status_change_direct(orders, order)
+            status_change_direct(orders, order, "収納依頼")
         convinience = True
     except:
-        pass
+        traceback.print_tb
     menu.close()
 
     if convinience and bank:
@@ -91,9 +91,9 @@ def mailto(to, subject, body):
         body=body
     )
 
-def status_change(orders, order, option):
+def status_change(orders, order, option, status):
     order.initialize()
-    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+    order.option_text(0, "・処理状況（テキスト）", status)
     order.option_list(1, "楽天ステータス", "発送待ち")
     order.input_order_id(orders)
     order.option_text(2,"・発注分類", option)
@@ -104,9 +104,9 @@ def status_change(orders, order, option):
         subwindow.find("id:cmbStatus").select("入金済み")
         confirming_change(order, subwindow)
 
-def status_change_direct(orders, order):
+def status_change_direct(orders, order, status):
     order.initialize()
-    order.option_text(0, "・処理状況（テキスト）", "入金待ち")
+    order.option_text(0, "・処理状況（テキスト）", status)
     order.option_list(1, "楽天ステータス", "発送待ち")
     order.input_order_id(orders)
     if order.search() > 0:
